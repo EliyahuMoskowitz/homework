25cc3a8c8fb98cb7694fbfef8fd3b98eeb885e90
EliyahuMoskowitzNew <Eliyahu>
2020-11-10 22:07:13 -0500

    76_test

--- js/75/ants.js
+++ js/75/ants.js
@@ -1,201 +1,262 @@
-//(function () {
-'use strict';
+(function () {
+    'use strict';
 
-const canvas = document.getElementById('theCanvas'), intervalLength = 100, home = document.getElementById('home');
-const homeX = 1250, homeY = 70, homeRadius = 50; let foodRestPlace = 0;
+    const canvas = document.getElementById('theCanvas'), intervalLength = 16.66,// home = document.getElementById('home');
+        homeRightX = 1250, homeRightY = 70, homeRightRadius = 50,
+        homeLeftX = 100, homeLeftY = 70, homeLeftRadius = 50, midCanvas = 675, lengthOfPopUp = 10;// let foodRestPlace = 0;
 
-function resizeCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-}
-window.addEventListener('resize', resizeCanvas);
-resizeCanvas();
-//let ants = [];
-
-class Ant {
-    // # experimental private so no one can change it - but makes jshint very unhappy
-    // static #SIZE = 2;
-
-    // js hint not ready for this experimental syntax yet
-    static SIZE = 2; static stepsMin = 15; static stepsMax = 35; static ants = []; static maxNoFood = 10000; // jshint ignore:line
-
-    constructor(context, color = '#000000') {
-        this.x = canvas.width / 2;
-        this.y = canvas.height / 2;
-        this.color = color;
-        this.context = context;
-        this.draw();
-        this.strength = 1;
-        this.fighting = false;
-        this.specific = false;
-        this.counter = 0;
-        this.specificX = Ant.getRandomNumber(-1, 1);
-        this.specificY = Ant.getRandomNumber(-1, 1);
-        this.steps = Ant.getRandomNumber(Ant.stepsMin, Ant.stepsMax);
-        this.goingHome = false;
-        this.noFood = 0;
-
-        // static without using experimental syntax
-        /*if (! Ant.SIZE) {
-          Ant.SIZE = 2;
-        }*/
+    function resizeCanvas() {
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
     }
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
 
-    draw() {
-        this.context.beginPath();
-        this.context.fillStyle = this.color;
-        this.context.fillRect(this.x, this.y, Ant.SIZE, Ant.SIZE);
-    }
+    class Ant {
+        // # experimental private so no one can change it - but makes jshint very unhappy
+        // static #SIZE = 2;
 
-    move() {
-        if (!this.specific) {
-            this.x += Ant.getRandomNumber(-1, 1);
-            this.y += Ant.getRandomNumber(-1, 1);
-            if (++this.counter === this.steps) {
-                this.specific = true;
-                this.counter = 0;
-            }
-        } else if (this.goingHome) {
-            // this.x = home.style.left;
-            // this.y = home.style.top;
-            this.x = homeX;
-            this.y = homeY;
-            // this.goingHome = false;
-        }
-        else {
-            this.x += this.specificX;
-            this.y += this.specificY;
-            if (++this.counter === this.steps) {
-                this.specific = false;
-                this.counter = 0;
-                this.specificX = Ant.getRandomNumber(-1, 1);
-                this.specificY = Ant.getRandomNumber(-1, 1);
-                this.steps = Ant.getRandomNumber(Ant.stepsMin, Ant.stepsMax);
-            }
+        // js hint not ready for this experimental syntax yet
+        static SIZE = 2; static stepsMin = 15; static stepsMax = 35; static ants = []; static maxNoFood = 10000; // jshint ignore:line
+
+        constructor(context, color = '#000000') {
+            this.x = canvas.width / 2;  //homeX;
+            this.y = canvas.height / 2; //homeY;
+            this.color = color;
+            this.context = context;
+            this.draw();
+            this.strength = 1;
+            this.fighting = false;
+            this.isDead = false;
+            this.specific = false;
+            this.counter = 0;
+            this.specificX = Ant.getRandomNumber(-1, 1);
+            this.specificY = Ant.getRandomNumber(-1, 1);
+            this.steps = Ant.getRandomNumber(Ant.stepsMin, Ant.stepsMax);
+            this.goingHome = false;
+            this.wayHomeX = 0;
+            this.wayHomeY = 0;
+            this.randomX = 0;
+            this.randomY = 0;
+            this.noFood = 0;
+
+            // static without using experimental syntax
+            /*if (! Ant.SIZE) {
+              Ant.SIZE = 2;
+            }*/
         }
 
-
-        if (this.x < Ant.SIZE) {
-            this.x = Ant.SIZE;
-        } else if (this.x > canvas.width - Ant.SIZE) {
-            this.x = canvas.width - Ant.SIZE;
+        draw() {
+            this.context.beginPath();
+            this.context.fillStyle = this.color;
+            this.context.fillRect(this.x, this.y, Ant.SIZE, Ant.SIZE);
         }
 
-        if (this.y < Ant.SIZE) {
-            this.y = Ant.SIZE;
-        } else if (this.y > canvas.height - Ant.SIZE) {
-            this.y = canvas.height - Ant.SIZE;
-        }
-
-        this.draw();
-        this.fight();
-
-        if (++this.noFood >= Ant.maxNoFood) {
-            Ant.ants = Ant.ants.filter(a => a !== this);
-        }
-    }
-    goHome() {
-
-    }
-
-    static getRandomNumber(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    fight() {
-        Ant.ants.forEach(a => {
-            if (a.x === this.x && a.y === this.y && a.color !== this.color && !this.fighting) {
-                console.log('before fight', Ant.ants.length);
-                this.fighting = true;
-                const fight = document.createElement('div'); fight.innerHTML = 'FIGHT'; fight.className = 'fight';
-                fight.style.top = `${this.y}px`; fight.style.left = `${this.x}px`;
-                document.body.appendChild(fight);
-                if (this.strength > a.strength) {
-                    this.strength += a.strength; console.log('this', this.strength);
-                    Ant.ants = Ant.ants.filter(ant => ant !== a);
-                } else if (this.strength < a.strength) {
-                    a.strength += this.strength; console.log('a', a.strength);
-                    Ant.ants = Ant.ants.filter(ant => ant !== this);
-                } else {
-                    let random = Ant.getRandomNumber(0, 1), remove;// ? this : a;
-                    if (random) { this.strength += a.strength; remove = this; } else { a.strength += this.strength; remove = a; }
-                    Ant.ants = Ant.ants.filter(ant => ant !== random);
+        move() {
+            if (!this.specific) {
+                this.x += Ant.getRandomNumber(-1, 1);
+                this.y += Ant.getRandomNumber(-1, 1);
+                if (++this.counter === this.steps) {
+                    this.specific = true;
+                    this.counter = 0;
                 }
-                setTimeout(() => fight.remove(), 2000);
-                this.fighting = false;
-                console.log('after fight', Ant.ants.length);
+            } else if (this.goingHome) {
+                this.x += this.wayHomeX;
+                this.y += this.wayHomeY;
+                // this.x = home.style.left;
+                // this.y = home.style.top;
+                // this.randomX = Ant.getRandomNumber(-homeRadius, homeRadius);
+                // this.randomY = Ant.getRandomNumber(-homeRadius, homeRadius);
+                // if (this.x === (homeX/* + this.randomX*/) && this.y === (homeY/* + this.randomY*/)) {
+                //     this.goingHome = false;
+                // }
             }
-        });
-    }
-}
+            else {
+                this.x += this.specificX;
+                this.y += this.specificY;
+                if (++this.counter === this.steps) {
+                    this.specific = false;
+                    this.counter = 0;
+                    this.specificX = Ant.getRandomNumber(-1, 1);
+                    this.specificY = Ant.getRandomNumber(-1, 1);
+                    this.steps = Ant.getRandomNumber(Ant.stepsMin, Ant.stepsMax);
+                }
+            }
 
-class Food {
-    static SIZE = 8; static foods = []    //jshint ignore:line
-    constructor(context, color = 'red') {
-        this.context = context;
-        this.color = color;
-        this.x = Ant.getRandomNumber(10, canvas.width - 10);
-        this.y = Ant.getRandomNumber(10, canvas.width - 10);
-        this.isHome = false;
-        this.draw();
-    }
-    draw() {
-        this.context.beginPath();
-        this.context.fillStyle = this.color;
-        this.context.fillRect(this.x, this.y, Food.SIZE, Food.SIZE);
-    }
-    captured() {
-        Ant.ants.forEach(ant => {
-            if (this.x === ant.x && this.y === ant.y && !this.isHome) {
-                console.log('captured', ant, this);
-                ant.goingHome = true;
-                ant.noFood = 0;
-                let intervalId = setInterval(() => {
-                    this.x = ant.x;
-                    this.y = ant.y;
-                    if (ant.x === /*home.style.left && ant.y === home.style.top*/homeX && ant.y === homeY) {
-                        clearInterval(intervalId);
-                        this.isHome = true;
-                        setTimeout(() => { ant.goingHome = false; }, 500);
+
+            if (this.x < Ant.SIZE) {
+                this.x = Ant.SIZE;
+            } else if (this.x > canvas.width - Ant.SIZE) {
+                this.x = canvas.width - Ant.SIZE;
+            }
+
+            if (this.y < Ant.SIZE) {
+                this.y = Ant.SIZE;
+            } else if (this.y > canvas.height - Ant.SIZE) {
+                this.y = canvas.height - Ant.SIZE;
+            }
+
+            this.draw();
+            this.fight();
+
+            if (++this.noFood >= Ant.maxNoFood) {
+                Ant.ants = Ant.ants.filter(a => a !== this);
+            }
+        }
+        // goHome() {
+        // }
+
+        static getRandomNumber(min, max) {
+            return Math.floor(Math.random() * (max - min + 1) + min);
+        }
+
+        fight() {
+            Ant.ants.forEach(a => {
+                if (a.x === this.x && a.y === this.y && a.color !== this.color && !this.fighting) {
+                    console.log('Ants before fight', Ant.ants.length);
+                    this.fighting = true;
+                    const fight = document.createElement('div'); fight.innerHTML = 'FIGHT'; fight.className = 'popUp';
+                    fight.style.top = `${this.y}px`; fight.style.left = `${this.x}px`;
+                    document.body.appendChild(fight);
+                    if (this.strength > a.strength) {
+                        this.strength += a.strength; console.log('this Winner has', this.strength, 'strength');
+                        Ant.ants = Ant.ants.filter(ant => ant !== a);
+                        a.isDead = true;
+                    } else if (this.strength < a.strength) {
+                        a.strength += this.strength; console.log('a Winner has', a.strength, 'strength');
+                        Ant.ants = Ant.ants.filter(ant => ant !== this);
+                        this.isDead = true;
+                    } else {
+                        let random = Ant.getRandomNumber(0, 1), remove;// ? this : a;
+                        if (random) {
+                            this.strength += a.strength; remove = a; console.log('this Winner has', a.strength, 'strength(was random)');
+                        } else { a.strength += this.strength; remove = this; console.log('a Winner has', a.strength, 'strength(was random)'); }
+                        Ant.ants = Ant.ants.filter(ant => ant !== remove);
+                        remove.isDead = true;
                     }
-                }, intervalLength);
-            }
-        });
+                    setTimeout(() => fight.remove(), intervalLength * lengthOfPopUp);
+                    this.fighting = false;
+                    console.log('Ants after fight', Ant.ants.length);
+                }
+            });
+        }
     }
-}
 
-const context = canvas.getContext('2d');
-// let ants = [];
-for (let i = 0; i < 200; i++) {
-    Ant.ants.push(new Ant(context));
-}
-for (let i = 0; i < 250; i++) {
-    Food.foods.push(new Food(context));
-}
+    class Food {
+        static SIZE = 8; static foods = []    //jshint ignore:line
+        constructor(context, color = 'red') {
+            this.context = context;
+            this.color = color;
+            this.x = Ant.getRandomNumber(10, canvas.width - 10);
+            this.y = Ant.getRandomNumber(10, canvas.width - 10);
+            this.isHome = false;
+            this.isCleared = false;
+            this.draw();
+        }
+        draw() {
+            this.context.beginPath();
+            this.context.fillStyle = this.color;
+            this.context.fillRect(this.x, this.y, Food.SIZE, Food.SIZE);
+        }
+        captured() {
+            Ant.ants.forEach(ant => {
+                if (this.x === ant.x && this.y === ant.y && !this.isHome && !ant.goingHome) {
+                    console.log('captured', ant, this);
+                    this.isHome = true;
+                    ant.goingHome = true;
+                    ant.noFood = 0;
 
-setInterval(() => {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    drawHome();
-    Ant.ants.forEach(ant => ant.move());
-    Food.foods.forEach(food => { food.draw(); food.captured(); });
-}, intervalLength);
+                    let theHomeX, theHomeY;
+                    if (this.x < midCanvas) {
+                        theHomeX = homeLeftX; theHomeY = homeLeftY;
+                    } else {
+                        theHomeX = homeRightX; theHomeY = homeRightY;
+                    }
 
-const amount = document.getElementById('amount'), add = document.getElementById('add'), antColor = document.getElementById('color');
-add.addEventListener('click', () => {
-    for (let i = 0; i < amount.value; i++) {
-        Ant.ants.push(new Ant(context, antColor.value));
+                    let intervalId = setInterval(() => {
+                        if (ant.isDead) { this.isHome = false; }
+                        if (this.isCleared) { ant.goingHome = false; }
+
+                        ant.wayHomeX = (ant.x - theHomeX) < 0 ? 1 : -1;
+                        ant.wayHomeY = (ant.y - theHomeY) < 0 ? 1 : -1;
+                        this.x = ant.x;
+                        this.y = ant.y; const discrepancy = 10;     ///*home.style.left && ant.y === home.style.top*/(homeX -10 /* + ant.randomX*/
+                        if (ant.x > (theHomeX - discrepancy) && ant.x < (theHomeX + discrepancy) && ant.y > (theHomeY - discrepancy) && ant.y < (theHomeY + discrepancy)) {
+                            console.log('before HOME', Food.foods.length);
+                            const eaten = document.createElement('div'); eaten.innerHTML = 'EATEN'; eaten.className = 'popUp';
+                            eaten.style.top = `${this.y}px`; eaten.style.left = `${this.x}px`;
+                            document.body.appendChild(eaten);
+                            setTimeout(() => eaten.remove(), intervalLength * lengthOfPopUp); this.isCleared = true;
+                            Food.foods = Food.foods.filter(f => f !== this);
+                            console.log('after HOME', Food.foods.length);
+                            ant.goingHome = false;
+                            clearInterval(intervalId);
+                        }
+                    }, intervalLength);
+                }
+            });
+        }
     }
-});
 
-function drawHome() {
-    context.lineWidth = 7;
-    context.beginPath();
-    context.arc(homeX, homeY, homeRadius, 0, 2 * Math.PI, true);
-    context.strokeStyle = 'blue';
-    context.fillStyle = 'yellow';
-    context.stroke();
-    context.fill();
-}
+    const context = canvas.getContext('2d');
+    const initialAnts = 200, initialFood = 250;// let ants = [];
+    for (let i = 0; i < initialAnts; i++) {
+        Ant.ants.push(new Ant(context));
+    }
+    for (let i = 0; i < initialFood; i++) {
+        Food.foods.push(new Food(context));
+    }
+
+    setInterval(() => {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        drawHome();
+        Ant.ants.forEach(ant => ant.move());
+        Food.foods.forEach(food => { food.draw(); food.captured(); });
+    }, intervalLength);
+
+    const amount = document.getElementById('amount'), add = document.getElementById('add'), antColor = document.getElementById('color');
+    add.addEventListener('click', () => {
+        for (let i = 0; i < amount.value; i++) {
+            Ant.ants.push(new Ant(context, antColor.value));
+        }
+    });
+
+    const amountFood = document.getElementById('amountFood'), addFood = document.getElementById('addFood'), foodColor = document.getElementById('colorFood');
+    addFood.addEventListener('click', () => {
+        for (let i = 0; i < amountFood.value; i++) {
+            Food.foods.push(new Food(context, foodColor.value));
+        }
+    });
+
+/*if(Food.foods){*/document.getElementById('clearFood').addEventListener('click', () => { Food.foods.forEach(f => f.isCleared = true); Food.foods = []; });
+/*if(Ant.ants){*/document.getElementById('clearAnts').addEventListener('click', () => {
+        Ant.ants.forEach(ant => ant.isDead = true);
+        Ant.ants = [];
+    });
+
+    function drawHome() {
+        context.lineWidth = 7;
+        context.beginPath();
+        context.arc(homeRightX, homeRightY, homeRightRadius, 0, 2 * Math.PI, true);
+        context.stroke();
+        context.strokeStyle = 'blue';
+        context.fillStyle = 'yellow';
+        context.stroke();
+        context.fill();
+        context.beginPath();
+        context.arc(homeLeftX, homeLeftY, homeLeftRadius, 0, 2 * Math.PI, true);
+        // context.fillRect(homeX, homeY, 10, 10);
+        // context.beginPath();
+        // context.moveTo(homeX - 20, homeY);
+        // context.lineTo(homeX - 10, homeY);
+        // context.lineTo(homeX, homeY);
+        // context.lineTo(homeX + 15, homeY);
+        context.stroke();
+        context.strokeStyle = 'blue';
+        context.fillStyle = 'yellow';
+        context.stroke();
+        context.fill();
+    }
 
 
-//}());
+}());
---
+++ js/76/76.js
@@ -0,0 +1,80 @@
+(function () {
+    'use strict';
+
+    class Item {
+        constructor(name, price, quantity) {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+
+    class Order {
+        constructor(name, address, items) {
+            this.name = name;
+            this.address = address;
+            this.items = items;
+        }
+
+        get total() {
+            let t = 0;
+            this.items.forEach(i => t += (i.price * i.quantity));
+            return t;
+        }
+
+    }
+
+    let ordersList = [];
+    fetch('orders.json')
+        .then(r => {
+            let { ok, status: s, statusText: st } = r;
+            if (!ok) {
+                throw new Error(`Sorry! ${s} which means ${st}`);
+            }
+            return r.json();
+        })
+        .then(orders => {
+            orders.forEach(/*o*/({ items, customer: name, address }) => {
+                // console.log(orders, o);
+                let itemsList = [];
+                //o.items.forEach(i => itemsList.push(new Item(i.item, i.total / i.quantity, i.quantity)));
+                //ordersList.push(new Order(o.customer, o.address, itemsList));
+                items.forEach(/*i*/({ item, total, quantity: amount }) => itemsList.push(new Item(item, total / amount, amount)));
+                ordersList.push(new Order(name, address, itemsList));
+            });
+
+            ordersList.forEach((order, orderIndex) => {
+
+                let display = document.createElement('div');
+                display.className = 'display';
+                display.innerHTML = `Order #${orderIndex + 1}<section class="s1">Customer: ${order.name}</section>
+                        <section class="s2">Address: ${order.address}</section><section class="s3">Items Purchased</section>`;
+                order.items.forEach((item, itemIndex) => {
+                    let mainItems = document.createElement('main'); mainItems.innerHTML = `Item ${itemIndex + 1}: 
+                        <section class="m1">${item.name}</section> <section class="m2">Price: $${rightPad(item.price, '0', 2)}</section>
+                        <section class="m3">Quantity: ${item.quantity} `;
+                    display.appendChild(mainItems);
+                });
+                let orderTotal = document.createElement('aside');
+                orderTotal.innerHTML = `</section> <section class="m4_total">TOTAL: $${rightPad(order.total, '0', 2)}</section>`;
+                display.appendChild(orderTotal);
+
+                document.body.appendChild(display);
+            });
+
+
+        }).catch(e => console.error(e));
+
+    function rightPad(nidon, padder, amount) {
+        let dec = JSON.stringify(nidon).split('.');
+        if (!dec[1]) {
+            dec[1] += '.00';
+        } else if (dec[1].length < amount) {
+            while (dec[1].length < amount) {
+                dec[1] += padder;
+            }
+        }
+        return `${dec[0]}.${dec[1]}`;
+    }
+
+}());
